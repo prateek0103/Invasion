2811dc35f1f5d49424de84f7a596320fed62390a
prateek0103 <prateek_gupta2012@vit.ac.in>
2014-11-06 22:31:42 +0530

    Game play improved

--- src/core.cpp
+++ src/core.cpp
@@ -40,6 +40,11 @@ struct CORE_BMPFileHeader
 	byte  numimportantcolors[4];
 };
 
+#define SND_MAX_SOURCES 8
+#define SND_MAX_LOOP_SOUNDS 2
+ALuint SND_Sources[SND_MAX_SOURCES];
+int    SND_NextSource = 0;
+
 //-----------------------------------------------------------------------------
 #define READ_LE_WORD(a)  (a[0] + a[1]*0x100)
 #define READ_LE_DWORD(a) (a[0] + a[1]*0x100 + a[2]*0x10000 + a[3]*0x1000000)
@@ -50,6 +55,12 @@ dword hp2(dword v) { v--; v |= v >> 1; v |= v >> 2; v |= v >> 4; v |= v >> 8; v 
 
 //-----------------------------------------------------------------------------
 static byte pixloadbuffer[2048 * 2048 * 4];
+
+GLuint CORE_GetBmpOpenGLTex(int texix)
+{
+	return g_textures[texix].tex;
+}
+
 int CORE_LoadBmp(const char filename[], bool wrap)
 {
 	GLint              retval = -1;
@@ -159,4 +170,188 @@ void CORE_RenderCenteredSprite(vec2 pos, vec2 size, int texix, rgba color, bool 
 	glTexCoord2d(g_textures[texix].w, g_textures[texix].h); glVertex2f(p1.x, p1.y);
 	glTexCoord2d(0.0, g_textures[texix].h); glVertex2f(p0.x, p1.y);
 	glEnd();
+}
+
+bool CORE_InitSound()
+{
+	ALCcontext *context;
+	ALCdevice *device;
+
+	device = alcOpenDevice(NULL);
+	if (device)
+	{
+		context = alcCreateContext(device, NULL);
+		alcMakeContextCurrent(context);
+		alGenSources(SND_MAX_SOURCES, SND_Sources);
+		alGetError();
+		return true;
+	}
+	else
+		return false;
+
+}
+
+//-----------------------------------------------------------------------------
+void CORE_EndSound()
+{
+	ALCdevice *device;
+	ALCcontext *context;
+
+	context = alcGetCurrentContext();
+	device = alcGetContextsDevice(context);
+
+	alDeleteSources(SND_MAX_SOURCES, SND_Sources);
+
+	alcMakeContextCurrent(NULL);
+	alcDestroyContext(context);
+	alcCloseDevice(device);
+}
+
+//-----------------------------------------------------------------------------
+void CORE_PlaySound(ALuint snd, float volume, float pitch)
+{
+	// Stop and play!
+	alSourceStop(SND_Sources[SND_NextSource]);
+	alSourcei(SND_Sources[SND_NextSource], AL_BUFFER, snd);
+	alSourcef(SND_Sources[SND_NextSource], AL_GAIN, volume);
+	alSourcef(SND_Sources[SND_NextSource], AL_PITCH, pitch);
+	alSourcePlay(SND_Sources[SND_NextSource]);
+
+	SND_NextSource++;
+	if (SND_NextSource == SND_MAX_SOURCES - SND_MAX_LOOP_SOUNDS)
+		SND_NextSource = 0;
+}
+
+//-----------------------------------------------------------------------------
+void CORE_PlayLoopSound(unsigned loopchannel, ALuint snd, float volume, float pitch)
+{
+	if (loopchannel >= SND_MAX_LOOP_SOUNDS)
+		return;
+	alSourceStop(SND_Sources[SND_MAX_SOURCES - loopchannel]);
+	alSourcei(SND_Sources[SND_MAX_SOURCES - loopchannel], AL_BUFFER, snd);
+	alSourcef(SND_Sources[SND_MAX_SOURCES - loopchannel], AL_GAIN, volume);
+	alSourcei(SND_Sources[SND_MAX_SOURCES - loopchannel], AL_LOOPING, AL_TRUE);
+	alSourcePlay(SND_Sources[SND_MAX_SOURCES - loopchannel]);
+}
+
+//-----------------------------------------------------------------------------
+void CORE_SetLoopSoundParam(unsigned loopchannel, float volume, float pitch)
+{
+	if (loopchannel >= SND_MAX_LOOP_SOUNDS)
+		return;
+	alSourcef(SND_Sources[SND_MAX_SOURCES - loopchannel], AL_GAIN, volume);
+	alSourcef(SND_Sources[SND_MAX_SOURCES - loopchannel], AL_PITCH, pitch);
+}
+
+//-----------------------------------------------------------------------------
+void CORE_StopLoopSound(unsigned loopchannel)
+{
+	alSourceStop(SND_Sources[SND_MAX_SOURCES - loopchannel]);
+}
+
+//-----------------------------------------------------------------------------
+struct CORE_RIFFHeader
+{
+	byte  chunkID[4];   // 'RIFF'
+	byte  chunkSize[4];
+	byte  format[4];    // 'WAVE'
+};
+
+struct CORE_RIFFChunkHeader
+{
+	byte  subChunkID[4];
+	byte  subChunkSize[4];
+};
+
+struct CORE_WAVEFormatChunk
+{
+	byte  audioFormat[2];
+	byte  numChannels[2];
+	byte  sampleRate[4];
+	byte  byteRate[4];
+	byte  blockAlignp[2];
+	byte  bitsPerSample[2];
+};
+
+//-----------------------------------------------------------------------------
+#define MAX_WAV_SIZE 32*1024*1024 // Max 32Mb sound!
+static byte soundloadbuffer[MAX_WAV_SIZE];
+ALuint CORE_LoadWav(const char filename[])
+{
+	ALuint          retval = UINT_MAX;
+	CORE_RIFFHeader hdr;
+
+	int fd = _open(filename, O_RDONLY);
+	if (fd != -1)
+	{
+		_read(fd, &hdr, sizeof(hdr));
+
+		if (hdr.chunkID[0] == 'R' && hdr.chunkID[1] == 'I' && hdr.chunkID[2] == 'F' && hdr.chunkID[3] == 'F'
+			&& hdr.format[0] == 'W' && hdr.format[1] == 'A' && hdr.format[2] == 'V' && hdr.format[3] == 'E')
+		{
+			CORE_WAVEFormatChunk fmt;
+			memset(&fmt, 0, sizeof(fmt));
+
+			for (;;)
+			{
+				CORE_RIFFChunkHeader chunkhdr;
+				if (_read(fd, &chunkhdr, sizeof(chunkhdr)) < sizeof(chunkhdr))
+					break;
+
+				dword chunkdatasize = READ_LE_DWORD(chunkhdr.subChunkSize);
+
+				if (chunkhdr.subChunkID[0] == 'f' && chunkhdr.subChunkID[1] == 'm' && chunkhdr.subChunkID[2] == 't' && chunkhdr.subChunkID[3] == ' ')
+				{
+					_read(fd, &fmt, sizeof(fmt));
+					_lseek(fd, ((1 + chunkdatasize) & -2) - sizeof(fmt), SEEK_CUR); // Skip to next chunk
+				}
+				else if (chunkhdr.subChunkID[0] == 'd' && chunkhdr.subChunkID[1] == 'a' && chunkhdr.subChunkID[2] == 't' && chunkhdr.subChunkID[3] == 'a')
+				{
+					dword contentsize = chunkdatasize;
+					if (contentsize > sizeof(soundloadbuffer))
+						contentsize = sizeof(soundloadbuffer);
+
+					_read(fd, soundloadbuffer, contentsize);
+
+					bool valid = true;
+					ALsizei al_size = contentsize;
+					ALsizei al_frequency = READ_LE_DWORD(fmt.sampleRate);
+					ALenum al_format = (ALenum)-1;
+					if (READ_LE_WORD(fmt.numChannels) == 1)
+					{
+						if (READ_LE_WORD(fmt.bitsPerSample) == 8) al_format = AL_FORMAT_MONO8;
+						else if (READ_LE_WORD(fmt.bitsPerSample) == 16) al_format = AL_FORMAT_MONO16;
+						else                                            valid = false;
+					}
+					else if (READ_LE_WORD(fmt.numChannels) == 2)
+					{
+						if (READ_LE_WORD(fmt.bitsPerSample) == 8) al_format = AL_FORMAT_STEREO8;
+						else if (READ_LE_WORD(fmt.bitsPerSample) == 16) al_format = AL_FORMAT_STEREO16;
+						else                                            valid = false;
+					}
+
+					if (valid)
+					{
+						ALuint al_buffer = UINT_MAX;
+						alGenBuffers(1, &al_buffer);
+
+						alBufferData(al_buffer, al_format, soundloadbuffer, al_size, al_frequency);
+						retval = al_buffer;
+					}
+					break;
+				}
+				else
+					_lseek(fd, ((1 + READ_LE_DWORD(chunkhdr.subChunkSize)) & -2), SEEK_CUR); // Skip to next chunk
+			}
+		}
+		_close(fd);
+	}
+
+	return retval;
+}
+
+//-----------------------------------------------------------------------------
+void CORE_UnloadWav(ALuint snd)
+{
+	alDeleteBuffers(1, &snd);
 }
